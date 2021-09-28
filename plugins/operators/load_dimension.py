bbaee8fc47ff3042d32deb644952654221725a7d
Plaban Das <plaban.das01@gmail.com>
2021-09-28 09:08:41 +0530

    feat: Implement load dimension operator
    Implement load dimension operator
    
    Signed-off-by: Plaban Das <plaban.das01@gmail.com>

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -3,20 +3,34 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
-
+    """ Operator for loadingq
+    """
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
-
+        redshift_conn_id,
+        table,
+        select_data_query,
+        truncate=True,
+        *args, **kwargs
+    ):
+        """ Constructor
+        """
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.select_data_query = select_data_query
+        self.truncate = truncate
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        """ This method defines the main execution logic of the operator
+        """
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.truncate:
+            self.log.info('Delete existing data from table {} !!!'.format(self.table))
+            redshift.run("DELETE FROM {}".format(self.table))
+
+        self.log.info('Loading data in table {}'.format(self.table))
+        redshift.run("INSERT INTO {} {}".format(self.table, self.select_data_query))
