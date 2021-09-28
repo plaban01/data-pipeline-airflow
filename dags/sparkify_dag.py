902507665f219c914b9982e9c9fe42b553f2ef98
Plaban Das <plaban.das01@gmail.com>
2021-09-28 09:12:00 +0530

    feat: Configure tasks to load dimension tables
    Load the following dimension tables:
    User
    Song
    Artist
    Time
    
    Signed-off-by: Plaban Das <plaban.das01@gmail.com>

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -61,22 +61,34 @@ load_songplays_table = LoadFactOperator(
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='users',
+    select_data_query=SqlQueries.user_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='songs',
+    select_data_query=SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='artists',
+    select_data_query=SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='time',
+    select_data_query=SqlQueries.time_table_insert
 )
 
 run_quality_checks = DataQualityOperator(
